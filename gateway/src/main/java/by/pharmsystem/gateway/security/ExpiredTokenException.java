7ef1ab27636eca0d7099b6d14dd5ea20ead3fb2b
Eugeny <zhenyanovikov96@gmail.com>
2018-02-25 13:18:17 +0300

    fix exceptions

--- gateway/src/main/java/by/pharmsystem/gateway/security/ExpiredTokenException.java
+++ gateway/src/main/java/by/pharmsystem/gateway/security/ExpiredTokenException.java
@@ -5,17 +5,4 @@ public class ExpiredTokenException extends Exception {
     public ExpiredTokenException(){
         super();
     }
-
-    public ExpiredTokenException(String message){
-        super(message);
-    }
-
-    public ExpiredTokenException(Exception e){
-        super(e);
-    }
-
-    public ExpiredTokenException(String message, Exception e){
-        super(message, e);
-    }
-
 }
